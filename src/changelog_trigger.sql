3197ec3bee7ab2d2e7fa394be577c45d5f241fe5
Arisna <arisna@cakeplabs.com>
2023-06-26 14:28:27 +0800

    update changelog trigger

--- src/changelog_trigger.sql
+++ src/changelog_trigger.sql
@@ -5,20 +5,23 @@ USE `demo`$$
 DROP TRIGGER /*!50032 IF EXISTS */ `changelog_to_outbox`$$
 
 CREATE
-    /*!50017 DEFINER = 'root'@'%' */
     TRIGGER `changelog_to_outbox` AFTER INSERT ON `changelog` 
-
     FOR EACH ROW BEGIN
-
                 DECLARE label VARCHAR(5) DEFAULT 'INS';
+                DECLARE blocklist VARCHAR(100) DEFAULT NULL;
                 IF NEW.type = 2 THEN
                     SET label = 'UPD';
                 ELSEIF NEW.type = 3 THEN
                     SET label = 'DEL';
                 END IF;
-                INSERT INTO outbox(`query`, `table`, `pk`, `prev_pk`, `type`, `label`) VALUES (NEW.query, NEW.table, NEW.pk, NEW.prev_pk, NEW.type, label);
+				IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'demo' AND TABLE_NAME = 'clients') THEN
+					SELECT id_sender INTO blocklist FROM inbox WHERE `table` = NEW.table AND `type` = NEW.type AND `query` = NEW.query AND is_process = 1 ORDER BY created_at DESC LIMIT 1;
+					IF blocklist IS NOT NULL THEN
+						SET blocklist = CONCAT('.', blocklist);
+					END IF;
+				END IF;
+				INSERT INTO outbox(`query`, `table`, `pk`, `prev_pk`, `type`, `label`, `block_list`) VALUES (NEW.query, NEW.table, NEW.pk, NEW.prev_pk, NEW.type, label, blocklist);
 			END;
 $$
-
 
 DELIMITER ;
